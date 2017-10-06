646e61e1b2780ee511eb21ace77efe616df7ffa5
Aaron Zhang <kingmanzhang@gmail.com>
2017-10-06 13:07:26 -0400

    format text

--- resource/utils/XML_parser.py
+++ resource/utils/XML_parser.py
@@ -17,13 +17,15 @@ def extract_text_from_p(element):
     :param element: should be an element with a tag of "p"
     :return: all texts under the element
     """
+    #
+
     return ''.join(element.itertext())
 
 
 def extract_sec(section):
     """ Extract information at the second bottom level
 
-    The bottom level is <title>, <p> with title being categories of treatment and p being descriptions.
+    The bottom level is <title>, <p>, <list> with title being categories of treatment and p being descriptions.
     One level above it is the entire "Management" section, as well as "Diagnosis", "Clinical_Characteristics",
     "Genetically_Related_Disorders", "Differential_Diagnosis", "Genetic_Counseling", etc. The function should
     work for all those sections (TO be tested).
@@ -34,7 +36,7 @@ def extract_sec(section):
     extractedText = ''
     for child in section.iter():
         if (child.tag == 'p'):
-            extractedText = extractedText + " " + extract_text_from_p(child).rstrip()
+            extractedText = extractedText + "\n\n" + extract_text_from_p(child).rstrip()
         if (child.tag == 'title'):
             extractedText = extractedText + '\n\n' + child.text.lstrip() + '\n\n'
         if (child.tag == 'table-wrap'):
@@ -54,8 +56,13 @@ def extract_Management(filepath):
     disease = disease_name(filepath)
     extractedText = ''
     for elem in tree.iterfind('./book-part/body/sec'):
-        # The attrib for "Management" is "(name of disease).Management"
-        if (elem.get('id') == disease + ".Management"):
+        # The attrib for "Management" is "(name of disease).Management" (652 diseases)
+        # or "(name of disease).Management_1" (40 diseases)
+        # or "(name of disease).Acute_Management_of_a_Urea" (1 diseases, ucd-overview)
+        # or "(name of disease).Management_of_Nonsyndromic_R" (1 diseases, rp-overview)
+        if (elem.get('id') == disease + ".Management" or elem.get('id') == disease + ".Management_1" \
+            or elem.get('id') == disease + ".Acute_Management_of_a_Urea" \
+                    or elem.get('id') == disease + ".Management_of_Nonsyndromic_R"):
             extractedText = extract_sec(elem)
     return extractedText
 
@@ -69,7 +76,10 @@ def disease_name(filepath):
     :param filepath: location of the xml file
     :return: name of the disease
     """
-    return filepath.split('/')[2].split('.')[0]
+    path_split = filepath.split('/')
+    # disease name will be in the last item, such as 'yci.nxml'
+    name_pos = len(path_split) - 1
+    return path_split[name_pos].split('.')[0]
 
 
 if __name__ == "__main__":
@@ -82,10 +92,12 @@ if __name__ == "__main__":
     for file in files_to_parse:
         export_path = EXPORT_FOLDER + disease_name(file) + '.txt'
         extracted = extract_Management(file)
+
         lines = []
         for line in extracted.split('\n'):
             lines.append(line.lstrip())
         extracted_formated = '\n'.join(lines)
+
         url = "http://www.ncbi.nlm.nih.gov/books/n/gene/" + disease_name(file)
         if (not extracted == ''):
             export = open(export_path, 'w+')
