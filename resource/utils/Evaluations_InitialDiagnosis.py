312544c470f43cf1c471b58d23ab3961a2db62c9
Aaron Zhang <kingmanzhang@gmail.com>
2017-10-06 15:31:13 -0400

    add function to get most frequent sentences

--- resource/utils/Evaluations_InitialDiagnosis.py
+++ resource/utils/Evaluations_InitialDiagnosis.py
@@ -1,4 +1,6 @@
 import glob
+from collections import OrderedDict
+import re
 
 
 def get_texts_between(filepath, start_line, end_line):
@@ -24,6 +26,17 @@ def get_texts_between(filepath, start_line, end_line):
     management.close()
     return '\n'.join(evaluation_initial_diagonosis)
 
+def get_short_sentences(filepath, threshold, short_sentences):
+    for line in iter(open(filepath)):
+        # replace all ';' and '.' to linebreaker so that they can be break into individual sentences
+        for sentence in line.replace(';', '\n').replace('.', '\n').split('\n'):
+                # for each sentence, get the number of words; put words <= threshold to dictionary
+            if sentence.strip() and len(sentence.strip()) > 2 and len(re.split('\W', sentence)) <= threshold:
+                if sentence.strip() in short_sentences.keys():
+                    short_sentences[sentence.strip()] = short_sentences[sentence.strip()] + 1
+                else:
+                    short_sentences[sentence.strip()] = 1
+
 
 def main():
     start_line = 'Evaluations Following Initial Diagnosis'
@@ -36,6 +49,19 @@ def main():
     for file in all_files:
         Evaluation_initial_Diagnosis.write(get_texts_between(file, start_line, end_line))
     Evaluation_initial_Diagnosis.close()
+
+    short_sentences = {}
+    filepath = '../Evaluation_initial_Diagnosis_all_disease.txt'
+    threshold = 10
+    get_short_sentences(filepath, threshold, short_sentences)
+    most_freqent_10 = OrderedDict(sorted(short_sentences.items(), key=lambda t: t[1], reverse=True))
+    try:
+        most_frequent = open('../most_frequent_phrase_10_words.txt', 'w')
+    except OSError:
+        print('Cannot create file')
+    for item in most_freqent_10:
+        most_frequent.write(str(item) + '\t'+ str(most_freqent_10[item]) + '\n')
+    most_frequent.close()
 
 
 if __name__ == '__main__':
