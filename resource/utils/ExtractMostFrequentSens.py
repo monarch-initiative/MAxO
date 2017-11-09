8edc18bf831d24b702ffd21593878bd7e168d472
Aaron Zhang <kingmanzhang@gmail.com>
2017-11-09 10:55:58 -0500

    not working for finding xref

--- resource/utils/ExtractMostFrequentSens.py
+++ resource/utils/ExtractMostFrequentSens.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 import re
 import os
 from XML_parser import disease_name, gene_NBKid
+from exist_OtherOntology import Ontology, ontologies_has_query
 
 
 def get_short_sentences(filepath, threshold, short_sentences):
@@ -55,6 +56,26 @@ def get_short_sentences_and_origin(filepath, threshold, short_sentences, origin)
                     short_sentences[sentence.strip()] = 1
 
 
+def get_short_sentences_and_xref(filepath, threshold, short_sentences, xref):
+
+    chebi = Ontology('../Other_ontologies/chebi.obo')
+    hp = Ontology('../Other_ontologies/hp.obo')
+    uberon = Ontology('../Other_ontologies/uberon.obo')
+    ontologies = {'chebi': chebi,
+                  'hp': hp,
+                  'uberon': uberon}
+
+    for line in iter(open(filepath)):
+        # replace all ';' and '.' to linebreaker so that they can be break into individual sentences
+        for sentence in line.lower().replace(';', '\n').replace('.', '\n').split('\n'):
+                # for each sentence, get the number of words; put words <= threshold to dictionary
+            if sentence.strip() and len(sentence.strip()) > 2 and len(re.split('\W', sentence)) <= threshold:
+                if sentence.strip() in xref.keys():
+                    short_sentences[sentence.strip()] = short_sentences[sentence.strip()] + 1
+                else:
+                    xref[sentence.strip()] = ontologies_has_query(ontologies, sentence.strip())
+                    short_sentences[sentence.strip()] = 1
+
 def dict_to_file(dict, path):
     """ Save a dictionary to a file
 
@@ -98,6 +119,16 @@ def frequent_sentences_and_origin(target_files, out, threshold=10):
     with open(out, 'w') as outfile:
         for (k, v) in short_sentences.items():
             outfile.write('\t'.join([k, str(v), '|'.join(sentence_origin[k]), '\n']))
+
+def frequent_sentences_and_xref(target_files, out, threshold=10):
+    short_sentences = {}
+    sentence_xref = {}
+    for file in target_files:
+        get_short_sentences_and_xref(file, threshold, short_sentences, sentence_xref)
+    short_sentences = order_dictionary(short_sentences)
+    with open(out, 'w') as outfile:
+        for (k, v) in short_sentences.items():
+            outfile.write('\t'.join([k, str(v), '|'.join(sentence_xref[k]), '\n']))
 
 
 
@@ -172,10 +203,34 @@ def get_ontology_terms_and_origin():
     target = ['../manual_parse_Evaluation_initial_Diagnosis_all_disease.text']
     print('Unalbe to find origin on manully parsed data')
 
+
 def get_ontology_terms_and_xref():
-    pass
+    try:
+        os.mkdir('../Ontology_Terms/Most_Frequent_Sens_and_Xref')
+    except FileExistsError:
+        pass
+    # get the most frequent sentences (no more than 10) of avoidance section
+
+    input_folders = ['Agents_Circumstances_to_Avoid', 'Evaluation_of_Relatives_at_Risk',
+                     'Evaluations_Following_Initial_Diagnosis', 'Prevention_of_Primary_Manifestations',
+                     'Prevention_of_Secondary_Complications', 'Surveillance', 'Treatment_of_Manifestations']
+    out_folder = '../Ontology_Terms/Most_Frequent_Sens_and_Xref/'
+    for input_folder in input_folders:
+        target_files = glob.glob('../Gene_Reviews_Extracted/' + input_folder + '/*.txt')
+        out_file = out_folder + 'most_frequent_10_' + input_folder + '.txt'
+        frequent_sentences_and_xref(target_files, out_file, threshold=10)
+
+    # get the most frequent sentences (no more than 10 words) in the entire Management section
+    target = glob.glob('../Gene_Reviews_Extracted/*.txt')
+    out = '../Ontology_Terms/Most_Frequent_Sens_and_Xref/most_frequent_10_entire_management.txt'
+    frequent_sentences_and_xref(target, out, threshold=10)
+
+    # get the most frequent sentences (no more than 10 words) in the entire Management section after some manual parsing
+    target = ['../manual_parse_Evaluation_initial_Diagnosis_all_disease.text']
+    print('Unalbe to find origin on manully parsed data')
+
 
 if __name__ == '__main__':
     get_ontology_terms()
     get_ontology_terms_and_origin()
-    get_ontology_terms_and_xref()
+    #get_ontology_terms_and_xref() # too show to run
