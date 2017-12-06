61eed33091d94dad1c692d02a4e7abf9caa93f02
Aaron Zhang <kingmanzhang@gmail.com>
2017-12-06 10:58:50 -0500

    update

--- src/ontology/module/avoid_foods.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dosdp-tools generate \
---obo-prefixes=true \
---prefixes=../pattern/prefixes.yaml \
---table-format=CSV \
---template=../pattern/avoid_consumption_of_foods.yaml \
---infile=avoid_foods.csv \
---outfile=avoid_consumption_of_foods.ofn
